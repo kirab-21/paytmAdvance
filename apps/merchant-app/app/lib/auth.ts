c330033673dd172398666d7e064ce4e7f296961e
Prabhudatta Barik <thexas21@gmail.com>
2025-02-21 01:19:30 +0530

    minor changes

--- apps/merchant-app/app/lib/auth.ts
+++ apps/merchant-app/app/lib/auth.ts
@@ -1,49 +1,74 @@
 import GoogleProvider from "next-auth/providers/google";
 import db from "@repo/db/client";
+import { z } from "zod";
+
+const userSchema = z.object({
+  email: z.string().email(),
+  name: z.string().min(2),
+});
+
+// const accountSchema = z.object ({
+//   provider
+// })
 
 export const authOptions = {
-    providers: [
-        GoogleProvider ({
-            clientId: process.env.GOOGLE_CLIENT_ID || "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
-        })
-    ],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
 
-    callbacks: {
-        async signIn({ user, account}: {
-            user: {
-                email: string;
-                name: string
-            },
-            account: {
-                providers:"google" | "github"
-            }
-        }) {
-            console.log("Hii signin")
-            if(!user || !user.email) {
-                return false;
-            }
+  callbacks: {
+    async signIn({
+      user,
+      account,
+    }: {
+      user: {
+        email: string;
+        name: string;
+      };
+      account: {
+        providers: "google" | "github";
+      };
+    }) {
+      console.log("Hii signin");
 
-            await db.merchant.upsert({
-                select: {
-                    id: true
-                },
-                where: {
-                    email: user.email
-                },
-                create: {
-                    email: user.email,
-                    name: user.name,
-                    auth_type: account.providers === "google" ? "Google" : "Github"
-                },
-                update: {
-                    name: user.name,
-                    auth_type: account.providers === "google" ? "Google" : "Github"
-                }
-            });
+      const userValidation = userSchema.safeParse(user);
+      if (!userValidation.success) {
+        console.error("Invalid user data", userValidation.error.errors);
+        return false;
+      }
 
-            return true;
-        }
+      // if (!user || !user.email) {
+      //   return false;
+      // }
+      //
+      try {
+        await db.merchant.upsert({
+          select: {
+            id: true,
+          },
+          where: {
+            email: user.email,
+          },
+          create: {
+            email: user.email,
+            name: user.name,
+            auth_type: account.provider === "google" ? "Google" : "Github",
+          },
+          update: {
+            name: user.name,
+            auth_type: account.provider === "google" ? "Google" : "Github",
+          },
+        });
+
+        return true;
+      } catch (error) {
+        console.error("Error in signIn callback:", error);
+        return false;
+      }
     },
-    secret: process.env.NEXTAUTH_SECRET || "secret"
-}
+  },
+  secret: process.env.NEXTAUTH_SECRET || "secret",
+};
--- apps/merchant-app/provider.tsx
+++ apps/merchant-app/provider.tsx
@@ -1,12 +1,20 @@
-"use client"
+"use client";
 
-import { RecoilRoot } from "recoil";
+//import { RecoilRoot } from "recoil";
+// import { SessionProvider } from "next-auth/react";
+
+// export const Provider = ({children}: {children: React.ReactNode}) => {
+//     return <RecoilRoot>
+//         <SessionProvider>
+//             {children}
+//         </SessionProvider>
+//     </RecoilRoot>
+// }
+
+//"use client";
+
 import { SessionProvider } from "next-auth/react";
 
-export const Provider = ({children}: {children: React.ReactNode}) => {
-    return <RecoilRoot>
-        <SessionProvider>
-            {children}
-        </SessionProvider>
-    </RecoilRoot>
-}
+export const Provider = ({ children }: { children: React.ReactNode }) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
